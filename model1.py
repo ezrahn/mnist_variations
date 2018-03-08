8097ed7a198daa383f481356fa135e85752e3748
Ezra Nugroho <ezrahn@gmail.com>
2018-03-07 22:43:40 -0800

    Add option to use full set or not

--- model1.py
+++ model1.py
@@ -5,15 +5,27 @@ from keras.optimizers import SGD
 
 import mnist
 
-train_images = mnist.train_images()[:5000]
-train_labels = keras.utils.to_categorical(mnist.train_labels()[:5000], num_classes=10)
-test_images = mnist.test_images()[:500]
-test_labels = keras.utils.to_categorical(mnist.test_labels()[:500], num_classes=10)
+use_full_set = True
+train_size = 5000
+test_size = 500
+epochs = 150
+batch_size = 128
+
+if use_full_set:
+       train_images = mnist.train_images()
+       train_labels = keras.utils.to_categorical(mnist.train_labels(), num_classes=10)
+       test_images = mnist.test_images()
+       test_labels = keras.utils.to_categorical(mnist.test_labels(), num_classes=10)
+else:
+       train_images = mnist.train_images()[:train_size]
+       train_labels = keras.utils.to_categorical(mnist.train_labels()[:train_size], num_classes=10)
+       test_images = mnist.test_images()[:test_size]
+       test_labels = keras.utils.to_categorical(mnist.test_labels()[:test_size], num_classes=10)
 
 model = Sequential()
-model.add(Dense(196, activation='sigmoid', input_shape=(28,28)))
+model.add(Dense(512, activation='sigmoid', input_shape=(28,28)))
 model.add(Dropout(0.2))
-model.add(Dense(98, activation='relu'))
+model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(Dense(10, activation='softmax'))
@@ -23,6 +35,6 @@ model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=20, batch_size=128)
-score = model.evaluate(test_images, test_labels, batch_size=128)
-                            
+model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size)
+score = model.evaluate(test_images, test_labels, batch_size=batch_size)
+print score
